add295b17c40710fe43ace7f8681fca4819f96f7
jspc <james@zero-internet.org.uk>
2013-12-09 17:05:47 +0000

    Correct option

--- lib/capistrano/ext/superusers.rb
+++ lib/capistrano/ext/superusers.rb
@@ -6,7 +6,7 @@ Capistrano::Configuration.class_eval do
     user_sudo   = fetch(:user_sudo,   "sudo -u #{owner} -i")
     ssh_forward = fetch(:ssh_forward, "setfacl -m #{owner}:rx $(dirname $SSH_AUTH_SOCK) && setfacl -m #{owner}:rwx $SSH_AUTH_SOCK")
 
-    if options[:keys]
+    if options[:key]
       run "#{ssh_forward} && #{user_sudo} #{default_shell} -c '#{cmd}'"
     else
       run "#{user_sudo} #{default_shell} -c '#{cmd}'"
