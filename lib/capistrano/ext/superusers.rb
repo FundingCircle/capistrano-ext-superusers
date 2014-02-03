ffd685e7044abffb090829032bd46cf5c2d2806c
jspc <james@zero-internet.org.uk>
2014-02-03 14:30:19 +0000

    Specify shell
    Otherwise this breaks with rvm

--- lib/capistrano/ext/superusers.rb
+++ lib/capistrano/ext/superusers.rb
@@ -5,11 +5,12 @@ Capistrano::Configuration.class_eval do
 
     user_sudo   = fetch(:user_sudo,   "sudo -u #{owner} -i")
     ssh_forward = fetch(:ssh_forward, "setfacl -m #{owner}:rx $(dirname $SSH_AUTH_SOCK) && setfacl -m #{owner}:rwx $SSH_AUTH_SOCK")
+    shell       = fetch(:user_shell,  :default_shell)
 
     if options[:key]
-      run "#{ssh_forward} && #{user_sudo} #{default_shell} -c '#{cmd}'"
+      run "#{ssh_forward} && #{user_sudo} #{shell} -c '#{cmd}'"
     else
-      run "#{user_sudo} #{default_shell} -c '#{cmd}'"
+      run "#{user_sudo} #{shell} -c '#{cmd}'"
     end
   end
 end
