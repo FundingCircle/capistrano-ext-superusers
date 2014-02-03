730a5dbfbce128a790d07d1238c5854229534bb1
jspc <james@zero-internet.org.uk>
2014-02-03 14:58:28 +0000

    Destructive gsub

--- lib/capistrano/ext/superusers.rb
+++ lib/capistrano/ext/superusers.rb
@@ -6,11 +6,13 @@ Capistrano::Configuration.class_eval do
     user_sudo   = fetch(:user_sudo,   "sudo -u #{owner} -i")
     ssh_forward = fetch(:ssh_forward, "setfacl -m #{owner}:rx $(dirname $SSH_AUTH_SOCK) && setfacl -m #{owner}:rwx $SSH_AUTH_SOCK")
     shell       = fetch(:user_shell,  :default_shell)
+    
+    cmd.gsub! "\n", ""
 
     if options[:key]
-      run "#{ssh_forward} && #{user_sudo} #{shell} -c '#{cmd.gsub("\n", '')}'", options
+      run "#{ssh_forward} && #{user_sudo} #{shell} -c '#{cmd}'", options
     else
-      run "#{user_sudo} #{shell} -c '#{cmd.gsub("\n", ''}'", options
+      run "#{user_sudo} #{shell} -c '#{cmd}'", options
     end
   end
 end
