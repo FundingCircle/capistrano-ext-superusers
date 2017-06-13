410198009d3b92d2a2c58098ba13f5d194e94e7d
Davide Dippolito <davide.dippolito@fundingcircle.com>
2017-06-13 11:20:14 +0100

    Update superusers to account for string immutability

--- lib/capistrano/ext/superusers.rb
+++ lib/capistrano/ext/superusers.rb
@@ -6,9 +6,7 @@ Capistrano::Configuration.class_eval do
     user_sudo   = fetch(:user_sudo,   "sudo -u #{owner} -i")
     ssh_forward = fetch(:ssh_forward, "setfacl -m #{owner}:rwx $(dirname $SSH_AUTH_SOCK) && setfacl -m #{owner}:rwx $SSH_AUTH_SOCK")
     shell       = fetch(:user_shell,  :default_shell)
-    
-    cmd.gsub! "\n", ""
 
-    run "#{ssh_forward} && #{user_sudo} #{shell} -c '#{cmd}'", options
+    run "#{ssh_forward} && #{user_sudo} #{shell} -c '#{cmd.gsub('\n', '')}'", options
   end
 end
